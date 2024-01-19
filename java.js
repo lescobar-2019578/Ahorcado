535300295be51dbc70111ac6bebfcfd4c4ca9599
lescobar-2019578 <lescobar-2019578@kinal.edu.gt>
2024-01-19 08:05:37 -0600

    segundo push, modificaciones en java

--- java.js
+++ java.js
@@ -1,1 +1,28 @@
-'Variables'
+/*   Variables  */ 
+
+var palabras = [["computadora","sexto","mause"]];
+
+var palabra = " ";
+// Aleatorio
+var rand;
+// Ocultar palabra
+var palOculta = [];
+//HTML de la palabra
+var espacio = document.getElementById("palabra");
+//intentos
+var cont = 6;
+// letras
+var buttons = document.getElementsByName('letra');
+//boton reiniciar
+var btnInicio = document.getElementById("reiniciar");
+
+/*    FUNCIONES     */ 
+
+function generaPalabra() {
+  rand = (Math.random() * 19).toFixed(0);
+  palabra = palabras[rand][0].toUpperCase();
+  console.log(palabra);
+}
+
+
+
